a47afd3e211e0e1ec89838758b853dc7e65eb179
Siddharth <siddharthrishabh019@gmail.com>
2025-06-21 03:10:55 +0530

    yet to update

---
+++ pattern/floyedTriangle.java
@@ -0,0 +1,10 @@
+public class floyedTriangle {
+    public static void main(String[] args){
+        for(int i=0; i<=5; i++){
+            for(int j = i+1; j<=j+1; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
