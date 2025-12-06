7a776a9f922a3ad6ecc66cf89bc021482bf49223
Siddharth <siddharthrishabh019@gmail.com>
2025-12-07 03:20:25 +0530

    Count Frequency Of Each Element using hashmap

---
+++ arrays/CountFrequencyOfEachElement.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountFrequencyOfEachElement {
+    int arr[] = { 1, 2, 1, 3, 2, 5, 4, 2 };
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public CountFrequencyOfEachElement(){
+   
+    for(
+    int num:arr)
+    {
+        map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    for(
+    Map.Entry<Integer, Integer> entry:map.entrySet())
+    {
+        System.out.println("Number " + entry.getKey() + " repeats " + entry.getValue() + " times");
+    }
+
+}     
+
+
+public static void main(String [] args){
+    new CountFrequencyOfEachElement();
+}
+;
+}
